0f670a2dc2942e24e45ccce320463ae21fa34748
Sahil Shore <sahilkhan08052002@gmail.com>
2025-06-19 13:30:54 +0530

    Removed large .pkl files and added gdown

--- app.py
+++ app.py
@@ -1,18 +1,33 @@
-import streamlit as st
+import os
+import gdown
 import pickle
 import pandas as pd
+import streamlit as st
 import requests
+
+# Google Drive File IDs
+MOVIES_FILE_ID = "1zUhTtkLmBMXqYa7voDBdu-KP0UBE4FXf"
+SIMILARITY_FILE_ID = "1PKEYeOlEhuI2q5HP08VrwwXAEnL99WQC"
+
+# Download from Google Drive if not already present
+if not os.path.exists("movies_dict.pkl"):
+    gdown.download(f"https://drive.google.com/uc?id={MOVIES_FILE_ID}", "movies_dict.pkl", quiet=False)
+
+if not os.path.exists("similarity.pkl"):
+    gdown.download(f"https://drive.google.com/uc?id={SIMILARITY_FILE_ID}", "similarity.pkl", quiet=False)
+
+# Load data
+movies_dict = pickle.load(open("movies_dict.pkl", "rb"))
+movies = pd.DataFrame(movies_dict)
+similarity = pickle.load(open("similarity.pkl", "rb"))
 
 def fetch_poster(movie_id):
     try:
-        response = requests.get(
-            "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id),
-            timeout=5
-        )
+        response = requests.get(f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US", timeout=10)
         data = response.json()
         return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
     except:
-        return "https://via.placeholder.com/500x750?text=No+Image"
+        return "https://via.placeholder.com/500x750?text=No+Poster"
 
 def Recommend(movie):
     movie_index = movies[movies['title'] == movie].index[0]
@@ -20,41 +35,24 @@ def Recommend(movie):
     movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
 
     recommend_movies = []
-    recommend_movies_poster = []
+    recommend_movies_posters = []
     for i in movies_list:
         movie_id = movies.iloc[i[0]].movie_id
         recommend_movies.append(movies.iloc[i[0]].title)
-        recommend_movies_poster.append(fetch_poster(movie_id))
-
-    return recommend_movies, recommend_movies_poster
-
-movies_dict = pickle.load(open('movies_dict.pkl', 'rb'))
-movies = pd.DataFrame(movies_dict)
-similarity = pickle.load(open('similarity.pkl', 'rb'))
+        recommend_movies_posters.append(fetch_poster(movie_id))
+    return recommend_movies, recommend_movies_posters
 
 st.title('Movie Recommender System')
 
-Salected_Movie_Name = st.selectbox(
-    'How would you like to be contacted',
-    movies['title'].values
-)
+selected_movie_name = st.selectbox(
+    'Select a movie to get recommendations:',
+    movies['title'].values)
 
 if st.button('Recommend'):
-    names, posters = Recommend(Salected_Movie_Name)
+    names, posters = Recommend(selected_movie_name)
 
-    col0, col1, col2, col3, col4 = st.columns(5)  # changed from st.beta_columns
-    with col0:
-        st.text(names[0])
-        st.image(posters[0])
-    with col1:
-        st.text(names[1])
-        st.image(posters[1])
-    with col2:
-        st.text(names[2])
-        st.image(posters[2])
-    with col3:
-        st.text(names[3])
-        st.image(posters[3])
-    with col4:
-        st.text(names[4])
-        st.image(posters[4])
+    cols = st.columns(5)
+    for i in range(5):
+        with cols[i]:
+            st.text(names[i])
+            st.image(posters[i])
